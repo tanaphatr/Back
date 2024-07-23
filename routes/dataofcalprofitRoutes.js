ab278579a313ddd94af3aa8a4ce66cd588fcb21d
tanaphatr <tanaphatrwangtong@gmail.com>
2024-07-23 18:37:32 +0700

    hu

--- routes/dataofcalprofitRoutes.js
+++ routes/dataofcalprofitRoutes.js
@@ -16,12 +16,14 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/post', async (req, res) => {
-    const {PriceList,ProDiscount_per,Margin_per,CusDiscount_per,Cost,PriceList_Margin,Customer_Price,Profit,Profit_per} = req.body;
+    const { PriceList, ProDiscount_per, Margin_per, CusDiscount_per, Cost, PriceList_Margin, Customer_Price, Profit, Profit_per } = req.body;
+    console.log(req.body)
+// ตรวจสอบค่าที่เหลือด้วย
 
     try {
         const result = await db.query(`
-            INSERT INTO data_of_calprofit (PriceList, ProDiscount_per, Margin_per, CusDiscount_per, Cost, PriceList_Margin, Customer_Price, Profit, Profit_per
-            ) VALUES ('${PriceList}', '${ProDiscount_per}', '${Margin_per}', '${CusDiscount_per}','${Cost}', '${PriceList_Margin}', '${Customer_Price}', '${Profit}', '${Profit_per}')`
+            INSERT INTO data_of_calprofit (PriceList, ProDiscount_per, Margin_per, CusDiscount_per, Cost, PriceList_Margin, Customer_Price, Profit, Profit_per)
+    VALUES ${PriceList.map((_, i) => `(${PriceList[i]}, ${ProDiscount_per[i]}, ${Margin_per[i]}, ${CusDiscount_per[i]}, ${Cost[i]}, ${PriceList_Margin[i]}, ${Customer_Price[i]}, ${Profit[i]}, ${Profit_per[i]})`).join(', ')}`
         );
         res.json(result);
     } catch (err) {
